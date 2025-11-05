8ae7ac1cce529ceefa57b8efd57771cbdf5e3d05
Rafael Nicholas Po <163509148+rafpoo@users.noreply.github.com>
2025-11-06 02:22:59 +0700

    feat: lab - send message function

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -6,19 +6,57 @@ public class CameraScript : MonoBehaviour
 {
     [SerializeField] AudioSource sumberSuara;
     [SerializeField] AudioClip clipMusik;
+    [SerializeField] AudioClip clipMusik2;
+
+    GameObject objCamera;
 
     // Start is called before the first frame update
     void Start()
     {
-        sumberSuara.clip = clipMusik;
-        sumberSuara.loop = true;
-        sumberSuara.volume = 0.8f;
-        sumberSuara.Play();
+        // sumberSuara.clip = clipMusik;
+        // sumberSuara.loop = true;
+        // sumberSuara.volume = 0.8f;
+        // sumberSuara.Play();
+
+        objCamera = transform.Find("Main Camera").gameObject;
     }
+
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "areaMusuh")
+        {
+            objCamera.SendMessage("rubahMusik1");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "areaMusuh")
+        {
+            objCamera.SendMessage("rubahMusik2");
+        }
+    }
+
+    public void rubahMusik1()
+    {
+        sumberSuara.Stop();
+
+        sumberSuara.clip = clipMusik2;
+        sumberSuara.Play();
+    }
+
+    public void rubahMusik2()
+    {
+        sumberSuara.Stop();
+
+        sumberSuara.clip = clipMusik;
+        sumberSuara.Play();
     }
 }
