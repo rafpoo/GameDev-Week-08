825ad99e9b2ea49f4d1b8c02447e80700e248fa2
Rafael Nicholas Po <163509148+rafpoo@users.noreply.github.com>
2025-11-14 14:09:20 +0700

    feat: lec - raycasting with destination

--- Assets/Scripts/HeroScript.cs
+++ Assets/Scripts/HeroScript.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class HeroScript : MonoBehaviour
 {
     float panjangRay = 10f;
+    Transform target;
     // Start is called before the first frame update
     void Start()
     {
-
+        target = GameObject.Find("target1").transform;
     }
 
     // Update is called once per frame
@@ -19,14 +20,15 @@ public class HeroScript : MonoBehaviour
         transform.Translate(new Vector3(0, 0, v) * 3f * Time.deltaTime);
         transform.Rotate(new Vector3(0, h, 0));
 
+        Vector3 arahTarget = target.position - transform.position;
         Ray ray = new Ray(
             transform.position,
-            transform.forward
+            arahTarget
         );
 
         Debug.DrawRay(
             transform.position,
-            transform.forward * panjangRay,
+            arahTarget * panjangRay,
             Color.red
         );
 
