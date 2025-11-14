c12bb84f4b0b6e2fad2ab835f417cd93c0f39521
Rafael Nicholas Po <163509148+rafpoo@users.noreply.github.com>
2025-11-14 14:18:29 +0700

    feat: lec - Raycasting 3d from camera with destination

--- Assets/Scripts/Lec/CameraRayCast.cs
+++ Assets/Scripts/Lec/CameraRayCast.cs
@@ -5,17 +5,20 @@ using UnityEngine;
 public class CameraRayCast : MonoBehaviour
 {
     float panjangRay = 100f;
+    Transform target;
     // Start is called before the first frame update
     void Start()
     {
-
+        target = GameObject.Find("target3").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
-        Debug.DrawLine(ray.origin, Camera.main.transform.forward * panjangRay, Color.red);
+        // Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        Vector3 arahTarget = target.position - transform.position;
+        Ray ray = new Ray(transform.position, arahTarget);
+        Debug.DrawLine(ray.origin, arahTarget * panjangRay, Color.red);
 
         RaycastHit hit;
         bool isRayHit = Physics.Raycast(ray, out hit, panjangRay);
