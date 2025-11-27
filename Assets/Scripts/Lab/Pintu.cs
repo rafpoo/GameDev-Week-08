f27422f96619725224cbb45f2bae3b387a91f32f
Rafael Nicholas Po <163509148+rafpoo@users.noreply.github.com>
2025-11-28 00:44:07 +0700

    feat: lab - ubah dikit

--- Assets/Scripts/Lab/Pintu.cs
+++ Assets/Scripts/Lab/Pintu.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class Pintu : MonoBehaviour
 {
+    Vector3 posPintuAwal;
+    Vector3 posPintuBuka;
+    GameObject hero;
     // Start is called before the first frame update
     void Start()
     {
-        
+        posPintuAwal = transform.position;
+
+        posPintuBuka = posPintuAwal;
+        posPintuBuka.x += 3;
+
+        hero = GameObject.Find("hero");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float jarakHero = Vector3.Distance(
+            hero.transform.position,
+            transform.position
+        );
+
+        if (jarakHero < 3)
+        {
+            transform.position = Vector3.Lerp(
+                transform.position,
+                posPintuBuka,
+                Time.deltaTime
+            );
+        }
+
+        if (jarakHero > 5)
+        {
+            transform.position = Vector3.Lerp(
+                transform.position,
+                posPintuAwal,
+                Time.deltaTime
+            );
+        }
     }
 }
